0af96126823f230c82084dc75e1c534a81925cca
Scott Leitstein <scott.leitstein@gmail.com>
2021-01-11 15:49:59 -0500

    YouTuber Tech With Tim - Full Stack Web App w/ Python and React Tutorial #6 - complete, really this time as this includes the event handlers. Handling POST Requests (Django REST) Material UI Components for the CreateRoomPage.js

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -9,13 +9,47 @@ import { Link } from "react-router-dom";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-import {FormLabel} from "@material-ui/core";
+import FormLabel from "@material-ui/core/FormLabel";
 
 export default class CreateRoomPage extends Component {
     defaultVotes = 2;
 
     constructor(props) {
         super(props);
+        this.state = {
+            guestCanPause: true,
+            votesToSkip: this.defaultVotes,
+        };
+
+        this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
+        this.handleVotesChange = this.handleVotesChange.bind(this);
+        this.handleCreateRoomButtonPressed = this.handleCreateRoomButtonPressed.bind(this);
+    }
+
+    handleVotesChange(e) {
+        this.setState({
+                votesToSkip: e.target.value,
+            });
+    }
+
+    handleGuestCanPauseChange(e) {
+        this.setState({
+            guestCanPause: e.target.value === "true",
+        });
+    }
+
+    handleCreateRoomButtonPressed() {
+        const requestOptions = {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                votes_to_skip: this.state.votesToSkip,
+                guest_can_pause: this.state.guestCanPause
+            }),
+        };
+        fetch('/api/create-room', requestOptions).then((response) =>
+            response.json()
+        ).then((data) => console.log(data));
     }
 
     render() {
@@ -33,7 +67,7 @@ export default class CreateRoomPage extends Component {
                                 Guest Control of Playback State
                             </div>
                         </FormHelperText>
-                        <RadioGroup row defaultValue="true">
+                        <RadioGroup row defaultValue="true" onChange={this.handleGuestCanPauseChange}>
                             <FormControlLabel value="true" control={<Radio color="primary" />}
                                               label="Play/Pause"
                                               labelPlacement="bottom"
@@ -47,6 +81,7 @@ export default class CreateRoomPage extends Component {
                     <Grid item xs={12} align="center">
                         <FormControl>
                             <TextField required={true} type="number"
+                                       onChange={this.handleVotesChange}
                                        defaultValue={this.defaultVotes}
                                        inputProps={ {
                                            min: 1,
@@ -59,7 +94,12 @@ export default class CreateRoomPage extends Component {
                         </FormControl>
                     </Grid>
                     <Grid item xs={12} align="center">
-                        <Button color="primary" variant="contained">Create A Room</Button>
+                        <Button color="primary"
+                                variant="contained"
+                                onClick={this.handleCreateRoomButtonPressed}
+                        >
+                            Create A Room
+                        </Button>
                     </Grid>
                     <Grid item xs={12} align="center">
                         <Button color="secondary" variant="contained" to="/" component={Link}>Back</Button>
